956f264c3499592534d5e24b621653f697800b61
steelthedev <akinwumikaliyanu@gmail.com>
2022-05-17 19:06:00 +0100

    added redirect to login for non authenticated

--- src/router/index.js
+++ src/router/index.js
@@ -129,4 +129,14 @@ const router = createRouter({
   routes
 })
 
+
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next({ name: 'LogIn', query: { to: to.path } });
+  } else {
+    next()
+  }
+})
+
 export default router
