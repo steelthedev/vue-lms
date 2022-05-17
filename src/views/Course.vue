7ce4073eec001ea6854fcfde0191cfbc566a09b1
steelthedev <akinwumikaliyanu@gmail.com>
2022-05-17 20:45:27 +0100

    Tried adding videos

--- src/views/Course.vue
+++ src/views/Course.vue
@@ -190,7 +190,7 @@ Page content START -->
 													<div class="position-relative d-flex align-items-center">
 														<a :href="lesson.get_video" class="btn btn-danger-soft btn-round btn-sm mb-0 stretched-link position-static">
 															<i class="fas fa-play me-0"></i>
-                              <video :src="lesson.get_video" controls>
+                              <video :src="lesson.get_video" controls></video>
 														</a>
 														<span class="d-inline-block text-truncate ms-2 mb-0 h6 fw-light w-100px w-sm-200px w-md-400px">Introduction</span>
 													</div>
